744da9268c5a2c30de271598f1d7f00dc19a7a4d
Ruinedmango <00rudystx@gmail.com>
2024-02-08 23:01:06 -0900

    Im boutta blow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn handle_arguments() {
+    let mut args : Vec<_> = std::env::args().collect(); // get all arguements passed to app
+    args.remove(0);
+    println!("{:?}", args);
+    if &args[0] == "cool" {
+        print!("Cool found");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    handle_arguments();
 }
