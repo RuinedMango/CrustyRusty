78754e082350a739c4d78ebe03c9f1619f4fca5f
Ruinedmango <00rudystx@gmail.com>
2024-02-09 13:56:49 -0900

    Implemented first cmd

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 fn handle_arguments() {
     let mut args : Vec<_> = std::env::args().collect(); // get all arguements passed to app
     args.remove(0);
-    println!("{:?}", args);
     if &args[0] == "cool" {
-        print!("Cool found");
+        print!("Cool found"); 
+        let mut cmd_args : Vec<String> = Vec::new();
+        cmd_args.push(args[1].clone());
+        cmd_args.push(args[2].clone());
+        cmd_args.push(args[3].clone());
+        cool(cmd_args);
     }
 }
 
 fn main() {
     handle_arguments();
+}
+
+fn cool(args: Vec<String>){
+    println!("{:?}", args[0]);
+    println!("{:?}", args[1]);
+    println!("{:?}", args[2]);
 }
